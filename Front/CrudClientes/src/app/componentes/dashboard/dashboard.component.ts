db3e6287f2fa74054b00cf05cd3710b77c9b8e66
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-04-27 15:07:00 -0300

    atts

--- Front/CrudClientes/src/app/componentes/dashboard/dashboard.component.ts
+++ Front/CrudClientes/src/app/componentes/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ClientesService } from 'src/app/services/clientes.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,13 +9,20 @@ import { ClientesService } from 'src/app/services/clientes.service';
 })
 export class DashboardComponent {
   clientes: any;
-  constructor(private clientesService: ClientesService) {
+  usuarios: any;
+  constructor(private clientesService: ClientesService, private usuariosService: UsuariosService) {
     this.reloadClientes()
+    this.reloadUsuarios()
   }
 
   reloadClientes() {
     this.clientesService.GetAll().subscribe((s) => {
       this.clientes = s;
+    })
+  }
+  reloadUsuarios() {
+    this.usuariosService.GetAll().subscribe((s) => {
+      this.usuarios = s;
     })
   }
 }
