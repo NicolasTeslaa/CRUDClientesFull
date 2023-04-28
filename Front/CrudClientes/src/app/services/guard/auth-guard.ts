a11475ceb2fc4d157da5f6ce49d7f534f38245da
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-04-28 13:48:07 -0300

    att

--- Front/CrudClientes/src/app/services/guard/auth-guard.ts
+++ Front/CrudClientes/src/app/services/guard/auth-guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 
@@ -8,7 +8,7 @@ import { AuthService } from '../auth.service';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -19,6 +19,7 @@ export class AuthGuard implements CanActivate {
       return true;
     } else {
       return false;
+      this.router.navigate(['/login'])
     }
 
   }
