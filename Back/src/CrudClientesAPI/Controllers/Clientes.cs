427d7a7c6b73944419a5089d7108db00635b408d
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-04-28 23:42:27 -0300

    atts

--- Back/src/CrudClientesAPI/Controllers/Clientes.cs
+++ Back/src/CrudClientesAPI/Controllers/Clientes.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CrudClientes.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("/api/[controller]")]
 public class Clientes : ControllerBase
 {
     private readonly Context _context;
--- Back/src/CrudClientesAPI/Program.cs
+++ Back/src/CrudClientesAPI/Program.cs
@@ -7,17 +7,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureSwaggerGen(setup =>
-{
-    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-    {
-        Title = "Crud Clientes",
-        Version = "v1"
-    });
-});
-
-
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "MyPolicy",
@@ -33,15 +22,14 @@ builder.Services.AddEntityFrameworkSqlServer()
     .AddDbContext<Context>(
         options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
-
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-// }
-app.UseSwagger();
-app.UseSwaggerUI();
 app.UseCors("MyPolicy");
 app.UseHttpsRedirection();
 app.UseAuthorization();
